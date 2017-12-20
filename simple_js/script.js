e0a26ea172168f278b9b2994a989b6d4467a34fe
egor-bogomolov <msicar.ot.on.yas@gmail.com>
2017-12-20 19:02:32 +0300

    Sample JS

---
+++ simple_js/script.js
@@ -0,0 +1,13 @@
+function changePicture() {
+    console.log(document.getElementById("demoPicture").style.display);
+    var display = document.getElementById("demoPicture").style.display;
+    if (display == "block") {
+        document.getElementById("demoPicture").style.display = "none";
+    } else {
+        document.getElementById("demoPicture").style.display = "block";
+    }
+}
+
+function calcSum() {
+
+}
